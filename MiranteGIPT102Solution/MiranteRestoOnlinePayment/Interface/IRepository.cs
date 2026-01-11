4675d3d25acf220ad61c96f429aa3adea488b2dd
gianmirante <143482154+gianmirante@users.noreply.github.com>
2026-01-11 18:56:23 +0800

    Add files via upload

---
+++ MiranteGIPT102Solution/MiranteRestoOnlinePayment/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiranteRestoOnlinePayment.Interface
+{
+    public class RestoOrder
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+       
+        public string PaymentMethod { get; set; }
+        public string TransactionStatus { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ MiranteGIPT102Solution/MiranteRestoOnlinePayment/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient; 
+using MiranteRestoOnlinePayment.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiranteRestoOnlinePayment
+{
+    public class RestoPaymentRepository
+    {
+        
+        private readonly string _connectionString = "Server=RestoServer;Database=RestoDB;Integrated Security=true;Encrypt=True;";
+
+       
+        public async Task<int> ExecutePaymentAsync(RestoOrder order)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                
+                string sql = @"INSERT INTO RestoOrders (CustomerName, TotalAmount, PaymentMethod, TransactionStatus, CreatedAt) 
+                           VALUES (@CustomerName, @TotalAmount, @PaymentMethod, 'Completed', GETDATE());
+                           SELECT CAST(SCOPE_IDENTITY() as int);";
+
+                return await db.ExecuteScalarAsync<int>(sql, order);
+            }
+        }
+
+       
+        public async Task<IEnumerable<RestoOrder>> GetCustomerTransactions(string name)
+        {
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                return await db.QueryAsync<RestoOrder>(
+                    "SELECT * FROM RestoOrders WHERE CustomerName = @CustomerName",
+                    new { CustomerName = name }
+                );
+            }
+        }
+    }
+}
